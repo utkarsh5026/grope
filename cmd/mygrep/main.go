55c18a016e47e898e82baca84f69ea1f11daa95b
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-08-05 19:39:06 +0530

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -10,10 +10,20 @@ import (
 	"unicode/utf8"
 )
 
+func matchLine(line []byte, pattern string) (bool, error) {
+	if utf8.RuneCountInString(pattern) != 1 {
+		return false, fmt.Errorf("unsupported pattern: %q", pattern)
+	}
+
+	var ok bool
+	ok = ContainsPattern(line, pattern)
+	return ok, nil
+}
+
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
-		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
+		_, _ = fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
 		os.Exit(2) // 1 means no lines were selected, >1 means error
 	}
 
@@ -21,34 +31,25 @@ func main() {
 
 	line, err := io.ReadAll(os.Stdin) // assume we're only dealing with a single line
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
 		os.Exit(2)
 	}
 
 	ok, err := matchLine(line, pattern)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(2)
 	}
 
 	if !ok {
 		os.Exit(1)
 	}
-
-	// default exit code is 0 which means success
 }
 
-func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) != 1 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
+func ContainsPattern(line []byte, pattern string) bool {
+	if pattern == "\\d" {
+		return bytes.ContainsAny(line, "0123456789")
 	}
 
-	var ok bool
-
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	ok = bytes.ContainsAny(line, pattern)
-
-	return ok, nil
+	return bytes.ContainsAny(line, pattern)
 }
